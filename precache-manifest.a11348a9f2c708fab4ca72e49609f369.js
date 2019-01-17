27560f782296d065888e617a18fbceb4ef91e53c
tokyosuite <helenyueyu@gmail.com>
2019-01-17 11:31:41 -0800

    Updates

---
+++ precache-manifest.a11348a9f2c708fab4ca72e49609f369.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5aafe58fd117a31370f",
+    "url": "/minesweeper2/static/css/main.d2a9df44.chunk.css"
+  },
+  {
+    "revision": "c5aafe58fd117a31370f",
+    "url": "/minesweeper2/static/js/main.c5aafe58.chunk.js"
+  },
+  {
+    "revision": "77a458057ff8f8a54ef7",
+    "url": "/minesweeper2/static/js/1.77a45805.chunk.js"
+  },
+  {
+    "revision": "7aa25acb0669ccb95a58",
+    "url": "/minesweeper2/static/js/runtime~main.7aa25acb.js"
+  },
+  {
+    "revision": "cba6e3437ebe2a7decb48e2dd41b8fb5",
+    "url": "/minesweeper2/index.html"
+  }
+];
